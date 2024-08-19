60a8d08668b7577f60aff13a4e2c336edc31acc5
sanxi <sanxigogo@gmail.com>
2024-08-19 10:45:46 +0800

    init pages

--- Index.py
+++ Index.py
@@ -6,42 +6,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
-st.markdown("""
-## introduction
-info here
-## getting started
-
-1. To install the workshop, follow these steps:
-```bash
-# install miniconda
-    ## mac
-    brew install miniconda
-    ## other systems
-    [miniconda installer](https://docs.conda.io/en/latest/miniconda.html)
-# Create a new conda environment with Python 3.10
-conda create -n py310 python=3.10
-
-# Activate the conda environment
-conda activate py310
-
-# Clone the repository to your local machine
-git clone  ...
-
-# Navigate to the project directory
-cd smart-marketing-llm-workshop
-
-# Install dependencies from requirements.txt
-pip install -r requirements.txt
-```
-
-2. After installing, you can launch the application simply by running:
-
-```bash
-streamlit run app.py
-```
-## workshop 
-1. [tanslate](/translate)
-2. [marketing note](/mkt_content)
-3. [agent](/agent)
-            """)
+# 读取 README.md 文件内容
+with open("README.md", "r", encoding="utf-8") as f:
+    readme_content = f.read()
+    # 在 Streamlit 应用程序中显示 README.md 内容
+    st.markdown(readme_content)
--- pages/2_💬mkt_content.py
+++ pages/2_💬mkt_content.py
@@ -1,2 +1,84 @@
 import streamlit as st
-st.title("index")
+import boto3
+import json
+
+st.set_page_config(
+    page_title="Marketing Note",
+    page_icon="💬",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# 创建Amazon Bedrock Runtime客户端
+client = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+
+# 定义翻译函数
+def translate(model_id, text, target_lang):
+    # Set the model ID, e.g., Claude 3 Haiku.
+
+    # Define the prompt for the model.
+    prompt = f"translate the text {text} to languate {target_lang}"
+
+    # Format the request payload using the model's native structure.
+    native_request = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 512,
+        "temperature": 0.5,
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": prompt}],
+            }
+        ],
+    }
+
+    request = json.dumps(native_request)
+
+    # 调用模型并获取响应流
+    streaming_response = client.invoke_model_with_response_stream(
+        modelId=model_id, body=request
+    )
+
+    for event in streaming_response["body"]:
+        chunk = json.loads(event["chunk"]["bytes"])
+        if chunk["type"] == "content_block_delta":
+            chunk_text = chunk["delta"].get("text", "")
+            yield chunk_text
+
+
+# Streamlit应用
+def app():
+    # 创建两列
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # 输入文本
+        st.markdown("**Source TEXT:**")
+        text = st.text_area("", height=200)
+
+        model_id = st.selectbox("Model", ["anthropic.claude-3-haiku-20240307-v1:0", "anthropic.claude-3-5-sonnet-20240620-v1:0"])
+        # 选择目标语言
+        target_lang = st.selectbox("Target", ["中文", "English", "français", "español", "Deutsch", "日本語", "한국어"])
+        btn_clicked = st.button("Translate")
+
+    with col2:
+        # 翻译文本
+        st.markdown(f"**{target_lang}:**")
+        result_container = st.container()
+        if btn_clicked:
+            with result_container:
+                st.markdown("""
+                    <style>
+                    .result-container {
+                        border: 1px solid #ccc;
+                        padding: 10px;
+                        border-radius: 5px;
+                    }
+                    </style>
+                """, unsafe_allow_html=True)
+                st.write_stream(translate(model_id, text, target_lang))
+
+
+if __name__ == "__main__":
+    app()
